f4cf320266e19a3487ecd0a3781f275f45488e37
warappa <spoiltnr1@gmx.at>
2025-12-16 20:49:07 +0100

    Fix nullable warning

--- src/BMEcatSharp/Types/EmailAddress.cs
+++ src/BMEcatSharp/Types/EmailAddress.cs
@@ -4,6 +4,17 @@
 [EditorBrowsable(EditorBrowsableState.Never)]
 public class EmailAddress : EmailComponent
 {
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public EmailAddress()
+    {
+        Value = null!;
+    }
+
+    public EmailAddress(string value)
+    {
+        Value = value;
+    }
+
     [XmlText]
     public string Value { get; set; }
 }
